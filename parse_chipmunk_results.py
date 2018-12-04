a4704644715708ebc14c9d0daad5377a4a869ae9
ubercomrade <ubercomrade@gmail.com>
2018-12-04 19:11:06 +0700

    new script

---
+++ parse_chipmunk_results.py
@@ -0,0 +1,232 @@
+'''
+Copyright © 2018 Anton Tsukanov. Contacts: tsukanov@bionet.nsc.ru
+License: http://www.gnu.org/licenses/gpl.txt
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
+
+import math
+import csv
+import sys
+import os
+import random
+import itertools
+import argparse
+import numpy as np
+import pandas as pd
+
+
+def get_sequences(path):
+    with open(path, 'r') as file:
+        output = []
+        for line in file:
+            d = {'name': str(), 'start': int(), 'end': int(),
+                 'seq': str(), 'strand': str()}
+            if line.startswith('WORD|'):
+                line = line[5:].strip()
+                line = line.split()
+                d['name'] = 'peaks_' + str(int(line[0]) - 1)
+                d['start'] = int(line[1])
+                d['end'] = int(line[1]) + len(line[2])
+                d['seq'] = line[2]
+                if line[4] == 'direct':
+                    d['strand'] = '+'
+                else:
+                    d['strand'] = '-'
+                output.append(d)
+            else:
+                continue
+    df = pd.DataFrame(output)
+    df = df[['name', 'start', 'end', 'strand', 'seq']]
+    return(df)
+
+
+def read_fasta(path):
+    sequences = []
+    with open(path, 'r') as file:
+        sequences = [i.strip().upper() for i in file if i.strip()[0] != '>']
+    return(sequences)
+
+
+def background_freq(seq):
+    s = ''.join(seq)
+    background = {}
+    mono_nucleotides = itertools.product('ACGT', repeat=1)
+    for i in mono_nucleotides:
+        background[i[0]] = s.count(i[0])
+    sum_of_nuc = sum(background.values())
+    for i in background.keys():
+        background[i] = background[i]/sum_of_nuc
+    return(background)
+
+
+def make_pfm_from_pcm(pcm, pseudocount='1/N'):
+    '''
+    Вычисление частотной матрицы на основе PCM.
+    Для того чтобы избавиться от 0 значений частот используется pseudocount.
+    Pseudocount может быть dict со стандартными значениями {'A':0.25, 'C': 0.25, 'G': 0.25, 'T': 0.25} [1],
+    либо pseudocount может быть str со значением sqroot [2].
+    Подробнее о расчетах смотри:
+    1)Wyeth W.Wasserman and Albin Sandelin
+      APPLIED BIOINFORMATICS FOR THE IDENTIFICATION OF REGULATORY ELEMENTS
+      doi:10.1038/nrg1315
+
+    2)Victor G Levitsky
+      Effective transcription factor binding site prediction using a
+      combination of optimization, a genetic algorithm and discriminant
+      analysis to capture distant interactions
+      doi:10.1186/1471-2105-8-481
+
+    В любых других условиях функция ничего не возвращает
+
+    '''
+
+    number_of_sites = [0] * len(pcm['A'])
+    for key in pcm.keys():
+        for i in range(len(pcm[key])):
+            number_of_sites[i] += pcm[key][i]
+
+    pfm = dict()
+    mono_nucleotides = itertools.product('ACGT', repeat=1)
+    for i in mono_nucleotides:
+        pfm[i[0]] = []
+
+    if pseudocount == '1/N':
+        first_key = list(pcm.keys())[0]
+        nuc_pseudo = 1/len(pcm.keys())
+        for i in range(len(pcm[first_key])):
+            for nuc in pcm.keys():
+                pfm[nuc].append((pcm[nuc][i] + nuc_pseudo) / (number_of_sites[i] + 1))
+        return(pfm)
+
+    elif pseudocount == 'sqroot':
+        total_sq_root = int()
+        for i in pcm.keys():
+            total_sq_root += pcm[i][0]
+        total_sq_root = math.sqrt(total_sq_root)
+        sq_root = total_sq_root/len(pcm.keys())
+
+        first_key = list(pcm.keys())[0]
+        for i in range(len(pcm[first_key])):
+            for nuc in pcm.keys():
+                pfm[nuc].append((pcm[nuc][i] + sq_root) / (number_of_sites[i] + total_sq_root))
+
+    return(pfm)
+
+
+def make_pwm_from_pcm(pcm, background, method='log-odds', pseudocount='1/N'):
+    '''
+    Функиця, которая считает PWM (position weight matrix) на основе PCM (position count matrix)
+    с преобразованием log-odds (добавить новые)
+
+    Ref:
+    1)Wyeth W.Wasserman and Albin Sandelin
+      APPLIED BIOINFORMATICS FOR THE IDENTIFICATION OF REGULATORY ELEMENTS
+      doi:10.1038/nrg1315
+
+    2)Victor G Levitsky
+      Effective transcription factor binding site prediction using a
+      combination of optimization, a genetic algorithm and discriminant
+      analysis to capture distant interactions
+      doi:10.1186/1471-2105-8-481
+
+    3)Oliver D. King and Frederick P. Roth
+      A non-parametric model for transcription factor binding sites
+      doi: 10.1093/nar/gng117
+
+    '''
+    pwm = {}
+    mono_nucleotides = itertools.product('ACGT', repeat=1)
+    for i in mono_nucleotides:
+        pwm[i[0]] = []
+
+    pfm = make_pfm_from_pcm(pcm, pseudocount)
+    first_key = list(pcm.keys())[0]
+    for i in range(len(pfm[first_key])):
+        for j in pfm.keys():
+            pwm[j].append(math.log(pfm[j][i] / background[j]))
+    return(pwm)
+
+
+def make_pcm(motifs):
+    '''
+    input - список мотивов одинаковой длины
+    output -  PCM
+    Создает PCM на основе списка мотивов
+    '''
+    matrix = {}
+    mono_nucleotides = itertools.product('ACGT', repeat=1)
+    for i in mono_nucleotides:
+        matrix[i[0]] = []
+    len_of_motif = len(motifs[0])
+    for i in matrix.keys():
+        matrix[i] = [0]*len_of_motif
+    for i in range(len_of_motif):
+        for l in motifs:
+            matrix[l[i]][i] += 1
+    return(matrix)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', action='store', dest='input',
+                        required=True, help='path to PCM matrix file HOCOMOCO format')
+    parser.add_argument('-o', '--output', action='store', dest='output',
+                        required=True, help='path to PWM output file')
+    parser.add_argument('-f', '--fasta', action='store', dest='fasta',
+                        required=False, help='path to BED file, needed to calculate backgroun frequences for nucleotieds. \
+                        Without this parametr background frequances = {A: 0.25, C: 0.25, G: 0.25, T: 0.25}')
+    return(parser.parse_args())
+
+
+def write_meme(output, tag, pcm, background, nsites):
+    with open(output + '/' + tag + '.meme', 'w') as file:
+        file.write('MEME version 4\n\nALPHABET= ACGT\n\nBackground letter frequencies\n')
+        file.write('A {0} C {1} G {2} T {3}\n\n'.format(background['A'], background['C'],
+                                                        background['G'], background['T']))
+        file.write('MOTIF {0}\n'.format(tag))
+        file.write('letter-probability matrix: alength= 4 w= {0} nsites= {1}\n'.format(inf['w']))
+        for i in zip(pfm['A'], pfm['C'], pfm['G'], pfm['T']):
+            file.write('{0}\t{1}\t{2}\t{3}\n'.format(i[0], i[1], i[2], i[3]))
+
+
+def write_pwm(output, tag, pwm):
+    with open(output + '/' + tag + '.pwm', 'w') as file:
+        file.write('>{0}\n'.format(tag))
+        for i in zip(pwm['A'], pwm['C'], pwm['G'], pwm['T']):
+            file.write('{0}\t{1}\t{2}\t{3}\n'.format(i[0], i[1], i[2], i[3]))
+
+
+def main():
+    args = parse_args()
+    input_ = args.input
+    output_ = args.output
+    background_path = args.background
+    tag = args.tag
+
+    seq = get_sequences(input_)
+    if not (fasta_path is None):
+        background = background_freq(list(seq['seq']))
+    else:
+        fasta = read_fasta(background_path)
+        background = background_freq(fasta)
+
+    pcm = make_pcm(list(seq['seq']))
+    pfm = make_pfm_from_pcm(pcm)
+    pwm = make_pwm_from_pcm(pcm, background)
+
+    if not os.path.isdir(output_):
+        os.mkdir(output_)
+
+
+# if __name__ == '__main__':
+#    main()
