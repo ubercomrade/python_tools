d74cd799c30de1190e7bb3299acae95150b2ec71
anton-tsukanov <tsukanov@bionet.nsc.ru>
2019-06-03 12:45:15 +0700

    mod

--- compare_sites3.py
+++ compare_sites3.py
@@ -269,13 +269,13 @@ def main():
     overlap_model_sites = third_model_sites.loc[third_model_sites['name'].searchsorted(np.array([index for index, i in enumerate(classification) if i == 'overlap_all_models']))]
 
     only_first_model_sites = only_first_model_sites[['chromosome', 'start', 'end', 'name', 'score', 'strand', 'site']]
-    only_first_model_sites.to_csv(out_dir + '/' + tag + '_all_first_model.sites', sep='\t', index=False, header=False)
+    only_first_model_sites.to_csv(out_dir + '/' + tag + '_only_first_model.sites', sep='\t', index=False, header=False)
 
     only_second_model_sites = only_second_model_sites[['chromosome', 'start', 'end', 'name', 'score', 'strand', 'site']]
-    only_second_model_sites.to_csv(out_dir + '/' + tag + '_all_second_model.sites', sep='\t', index=False, header=False)
+    only_second_model_sites.to_csv(out_dir + '/' + tag + '_only_second_model.sites', sep='\t', index=False, header=False)
 
     only_third_model_sites = only_third_model_sites[['chromosome', 'start', 'end', 'name', 'score', 'strand', 'site']]
-    only_third_model_sites.to_csv(out_dir + '/' + tag + '_all_third_model.sites', sep='\t', index=False, header=False)
+    only_third_model_sites.to_csv(out_dir + '/' + tag + '_only_third_model.sites', sep='\t', index=False, header=False)
 
     overlap_model_sites = overlap_model_sites[['chromosome', 'start', 'end', 'name', 'score', 'strand', 'site']]
     overlap_model_sites.to_csv(out_dir + '/' + tag + '_overlap_model_sites.sites', sep='\t', index=False, header=False)
